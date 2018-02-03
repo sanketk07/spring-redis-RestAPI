503f9df9c483a5fff3418036f8b2079b1d7a13c0
sanketk07 <sanketskumar@gmail.com>
2018-02-03 15:21:57 -0500

    Project starter files

---
+++ src/main/java/com/info7255/demo1/sanketbigdatademo1/SanketBigDataDemo1Application.java
@@ -0,0 +1,17 @@
+package com.info7255.demo1.sanketbigdatademo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class SanketBigDataDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanketBigDataDemo1Application.class, args);
+	}
+}
---
+++ src/test/java/com/info7255/demo1/sanketbigdatademo1/SanketBigDataDemo1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.info7255.demo1.sanketbigdatademo1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SanketBigDataDemo1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
